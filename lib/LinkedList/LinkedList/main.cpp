c269af53df5f16ce51d0b0d12d9bfa1cc4ea0769
timakin <timaki.st@gmail.com>
2015-03-10 12:45:33 +0900

    sigle List complete

--- lib/LinkedList/LinkedList/main.cpp
+++ lib/LinkedList/LinkedList/main.cpp
@@ -11,11 +11,11 @@ using namespace std;
 
 class Node {
     int data;
-    Node *next;
+    Node* next;
   public:
-    Node() {cout<<"Object Node created.\n";};
+    Node() {};
     void setData(int tmpData){ data = tmpData; };
-    void setNext(Node *tmpNext){ next = tmpNext; };
+    void setNext(Node* tmpNext){ next = tmpNext; };
     int Data() { return data; };
     Node* Next() { return next; };
 };
@@ -29,12 +29,107 @@ class List {
     void Delete(int data);
 };
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    Node* testnode = new Node();
-    testnode->next = 0;
-    testnode->data = 100;
+void List::Print() {
+    Node *tmp = head;
+    // 要素がない場合。
+    if (tmp == NULL) {
+        cout << "EMPTY" << endl;
+        return;
+    }
     
-    return testnode;
+    // 一個しかない場合。
+    if (tmp->Next() == NULL) {
+        cout << tmp->Data();
+        cout << "-->";
+        cout << "NULL" << endl;
+    } else {
+        // 複数ある場合。
+        do {
+            cout << tmp->Data();
+            cout << "-->";
+            tmp = tmp->Next();
+        } while (tmp != NULL);
+        cout << "NULL" << endl;
+    }
+};
+
+void List::Append(int data) {
+    // 次のノード
+    Node* newNode = new Node();
+    newNode->setData(data);
+    newNode->setNext(NULL);
+    
+    // 一時的にノードのアドレスをつくる。
+    // 初期状態はnull
+    Node *tmp = head;
+    if (tmp != NULL) {
+        // ノードがすでに存在していたら、
+        // 最後のノードにたどり着くまで移動
+        while(tmp->Next() != NULL) {
+            tmp = tmp->Next();
+        }
+        tmp->setNext(newNode);
+    } else {
+        // 上記のようなheadがnullの状態、つまり空の状態では、
+        // そのままheadにnewNodeを入れる。
+        head = newNode;
+    }
+};
+
+void List::Delete(int data) {
+    Node *tmp = head;
+    
+    // 初期状態の場合。何も消せないのでreturn
+    if (tmp == NULL) {
+        return;
+    }
+    
+    // １個しか値がない場合。
+    if (tmp->Next() == NULL) {
+        delete tmp;
+        head = NULL;
+    // 複数入ってる場合。
+    } else {
+        // データを
+        Node *prev;
+        do {
+            // 目標のデータがあったらよし
+            if (tmp->Data() == data) break;
+            prev = tmp;
+            tmp = tmp->Next();
+        } while (tmp!=NULL);
+        // 消したデータの次の値を、前の値につなぐ
+        prev->setNext(tmp->Next());
+        delete tmp;
+    }
+};
+
+int main()
+{
+    // New list
+    List list;
+    
+    // Append nodes to the list
+    list.Append(100);
+    list.Print();
+    list.Append(200);
+    list.Print();
+    list.Append(300);
+    list.Print();
+    list.Append(400);
+    list.Print();
+    list.Append(500);
+    list.Print();
+    
+    // Delete nodes from the list
+    list.Delete(400);
+    list.Print();
+    list.Delete(300);
+    list.Print();
+    list.Delete(200);
+    list.Print();
+    list.Delete(500);
+    list.Print();
+    list.Delete(100);
+    list.Print();
 }
