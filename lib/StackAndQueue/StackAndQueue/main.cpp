8f167799a6aaacf35b9b05a835bb03e7f3d29795
timakin <timaki.st@gmail.com>
2015-03-31 11:00:01 +0900

    queue

--- lib/StackAndQueue/StackAndQueue/main.cpp
+++ lib/StackAndQueue/StackAndQueue/main.cpp
@@ -45,6 +45,53 @@ void Stack::disp() {
         cout << vals[i] << endl;
 }
 
+
+#define QUEUE_MAX 5
+// [back]- ..[values].. -[front]
+class Queue {
+    int queue_front;
+    int queue_back;
+public:
+    Queue() {
+        queue_front = 0;
+        queue_back = 0;
+    }
+    void enqueue(int x);
+    int dequeue();
+    void disp();
+    int vals[];
+};
+
+void Queue::enqueue(int x) {
+    if ((queue_front + 1) % QUEUE_MAX == queue_back) {
+        cout << "Queue is fulfilled." << endl;
+    } else {
+        vals[queue_front] = x;
+        queue_front = (queue_front + 1) % QUEUE_MAX;
+    }
+}
+
+int Queue::dequeue() {
+    if (queue_back == queue_front) {
+        cout << "Queue is already empty." << endl;
+        return 0;
+    } else {
+        cout << vals[queue_back] << endl;
+        int result = vals[queue_back];
+        queue_back = (queue_back + 1) % QUEUE_MAX;
+        return result;
+    }
+}
+
+void Queue::disp() {
+    if (queue_back == queue_front) {
+        cout << "Queue is empty." << endl;
+    } else {
+        for(int i = queue_back; i < queue_front; i++)
+            cout << vals[i] << endl;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     Stack *test = new Stack();
     test->pop();
@@ -64,4 +111,18 @@ int main(int argc, const char * argv[]) {
     test->pop();
     test->pop();
     test->pop();
+    
+    
+    cout << "~~~~~~~~~~~" << endl;
+    Queue *testqueue = new Queue();
+    testqueue->enqueue(1);
+    testqueue->enqueue(2);
+    testqueue->enqueue(3);
+    testqueue->enqueue(4);
+    testqueue->enqueue(5);
+    cout << "~~~~~~~~~~~" << endl;
+    testqueue->dequeue();
+    cout << "~~~~~~~~~~~" << endl;
+    testqueue->disp();
+    
 }
